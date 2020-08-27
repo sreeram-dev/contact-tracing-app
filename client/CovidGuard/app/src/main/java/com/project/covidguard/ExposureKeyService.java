77cba8378092cfc7fbb7547b5f40589ab69b42f9
Amal Sailendran <amal19952011@gmail.com>
2020-08-27 16:10:45 +0930

    add bluetooth ON verification and location ON service verification. Add sample payload sending using Altbeacon

--- client/CovidGuard/app/src/main/java/com/project/covidguard/ExposureKeyService.java
+++ client/CovidGuard/app/src/main/java/com/project/covidguard/ExposureKeyService.java
@@ -41,6 +41,7 @@ public class ExposureKeyService extends Service {
 
     volatile static byte[] TEK;
     volatile static byte[] RPIKey;
+    volatile static byte[] rollingProximityID;
     static SecureRandom secureRandom;
     volatile static long ENIntervalNumber;
     private static final int SECS_PER_MIN = 60;
@@ -76,9 +77,8 @@ public class ExposureKeyService extends Service {
 
             byte[] info = "EN-RPIK".getBytes();
             TEK = new byte[]{-42, -103, -22, -10, 69, -70, 95, -67, 71, 2, 125, -3, -86, 68, -30, -58};
-//            bytesTemp = secureRandom.generateSeed(16);
+//            TEK = secureRandom.generateSeed(16);
             RPIKey = HKDF.fromHmacSha256().expand(TEK, info, 16);
-
             Log.d("TEK", Arrays.toString(TEK));
             Log.d("RPIKey", Arrays.toString(RPIKey));
             aesKey = new SecretKeySpec(RPIKey, 0, 16, "AES");
@@ -87,32 +87,34 @@ public class ExposureKeyService extends Service {
         }
     }
 
-    private static class RPIGenerator implements Runnable {
+    private class RPIGenerator implements Runnable {
 
         @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void run() {
             try {
+
                 byte[] paddedData = new byte[16];
 
-                System.arraycopy("EN-RPI".getBytes(StandardCharsets.UTF_8), 0, paddedData, 0, "EN-RPI".length());//                byte[] RPI = AES.main(outputKeyingMaterial, paddedData);
-
-
+                System.arraycopy("EN-RPI".getBytes(StandardCharsets.UTF_8), 0, paddedData, 0, "EN-RPI".length());
                 ENIntervalNumber = getENIntervalNumber(System.currentTimeMillis() / 1000);
-
                 paddedData[12] = (byte) (ENIntervalNumber & 0xFF);
                 paddedData[13] = (byte) (ENIntervalNumber >> 8 % 0xFF);
                 paddedData[14] = (byte) (ENIntervalNumber >> 16 % 0xFF);
                 paddedData[15] = (byte) (ENIntervalNumber >> 24 % 0xFF);
-
-
                 cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-
-                byte[] rollingProximityID = cipher.doFinal(paddedData);
+                rollingProximityID = cipher.doFinal(paddedData);
                 Log.d("ENIntervalNumber", String.valueOf(ENIntervalNumber));
                 Log.d("RPI", Arrays.toString(rollingProximityID));
-//                Log.d("MilliSeconds", String.valueOf(((System.currentTimeMillis()/1000))/60*10));
-
+                String uuidString = "01020304-0506-0708-090a-0b0c0d0e0f10";
+                BeaconParser beaconParser = new BeaconParser()
+                        .setBeaconLayout("s:0-1=fd6f,p:-:-59,i:2-17");
+                Beacon beacon = new Beacon.Builder()
+                        .setId1(uuidString) //currently hardcoded, no data transmission but beacon detection works
+                        .build();
+                BeaconTransmitter beaconTransmitter = new
+                        BeaconTransmitter(getApplicationContext(), beaconParser);
+                beaconTransmitter.startAdvertising(beacon);
             } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
                 Log.d("RPI", "Issue");
                 e.printStackTrace();
@@ -143,26 +145,17 @@ public class ExposureKeyService extends Service {
         String input = intent.getStringExtra("inputExtra");
         tekGenerator = new TEKGenerator();
         rpiGenerator = new RPIGenerator();
-        scheduleTaskExecutor.scheduleAtFixedRate(tekGenerator, 0, 60, TimeUnit.MINUTES);
-        scheduleTaskExecutor.scheduleAtFixedRate(rpiGenerator, 0, 10, TimeUnit.MINUTES);
-        String uuidString = "01020304-0506-0708-090a-0b0c0d0e0f10";
-        Beacon beacon = new Beacon.Builder()
-                .setId1(uuidString)
-                .build();
-        /*
+        scheduleTaskExecutor.scheduleAtFixedRate(tekGenerator, 0, 60, TimeUnit.SECONDS );
+        scheduleTaskExecutor.scheduleAtFixedRate(rpiGenerator, 0, 10, TimeUnit.SECONDS);
+         /*
         This beacon layout is for the Exposure Notification service Bluetooth Spec
         That layout string above is what tells the library how to understand this new beacon type.
         The layout “s:0-1=fd6f,p:-:-59,i:2-17,d:18-21” means that the advertisement is a gatt service type (“s:”) with a 16-bit service UUID of 0xfd6f (“0-1=fd6f”)
         and it has a single 16-byte identifier in byte positions 2-17 of the advertisement (“i:2-17”)
-        and a 4-byte data field in positions 18-21 (“d:18-21”).
         The “p:-:-59” indicates that there is no unencrypted measured power calibration reference transmitted with this beacon,
         and the library should default to using a 1-meter reference of -59 dBm for its built-in distance estimates.
          */
-        BeaconParser beaconParser = new BeaconParser()
-                .setBeaconLayout("s:0-1=fd6f,p:-:-59,i:2-17");
-        BeaconTransmitter beaconTransmitter = new
-                BeaconTransmitter(getApplicationContext(), beaconParser);
-        beaconTransmitter.startAdvertising(beacon);
+
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,
