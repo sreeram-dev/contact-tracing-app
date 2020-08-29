510441a2e9cea92f96cb9bd9b750390576a95011
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2020-08-29 14:35:20 +0930

    Add constructor for error response

--- client/CovidGuard/app/src/main/java/com/project/covidguard/web/responses/ErrorResponse.java
+++ client/CovidGuard/app/src/main/java/com/project/covidguard/web/responses/ErrorResponse.java
@@ -4,4 +4,10 @@ public class ErrorResponse {
     public String code;
     public String name;
     public String description;
+
+    public ErrorResponse(String code, String name, String description) {
+        this.code =  code;
+        this.name = name;
+        this.description = description;
+    }
 }
