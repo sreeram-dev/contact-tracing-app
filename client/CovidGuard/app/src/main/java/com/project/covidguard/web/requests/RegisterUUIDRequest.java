b3f0671095644d637163491876c022a0209cd7fa
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2020-08-29 17:06:14 +0930

    Add material Design

--- client/CovidGuard/app/src/main/java/com/project/covidguard/web/requests/RegisterUUIDRequest.java
+++ client/CovidGuard/app/src/main/java/com/project/covidguard/web/requests/RegisterUUIDRequest.java
@@ -1,6 +1,10 @@
 package com.project.covidguard.web.requests;
 
-
 public class RegisterUUIDRequest {
     public String uuid;
+
+
+    public RegisterUUIDRequest(String uuid) {
+        this.uuid = uuid;
+    }
 }
