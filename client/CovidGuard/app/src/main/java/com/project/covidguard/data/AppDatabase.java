23b5a9c2427e62d29e6f12cdc0b40b8a4143db1b
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2020-10-30 14:49:50 +1030

    Add SQLCipher database encryption

--- client/CovidGuard/app/src/main/java/com/project/covidguard/data/AppDatabase.java
+++ client/CovidGuard/app/src/main/java/com/project/covidguard/data/AppDatabase.java
@@ -2,16 +2,26 @@ package com.project.covidguard.data;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.project.covidguard.R;
+import com.project.covidguard.StorageUtils;
 import com.project.covidguard.data.dao.RPIDao;
 import com.project.covidguard.data.dao.TEKDao;
 import com.project.covidguard.data.entities.RPI;
 import com.project.covidguard.data.entities.TEK;
+
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SupportFactory;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.UUID;
 
 /**
  * SQLite Database
@@ -27,18 +37,46 @@ public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase sInstance;
 
     public static AppDatabase getDatabase(Context context) {
+
+
+
         if (sInstance == null) {
             synchronized (LOCK) {
                 Log.d(LOG_TAG, "Creating new database instance");
+                String passphrase;
+                try {
+                    passphrase = getPassphraseFromSharedPreferences(context);
+                } catch (GeneralSecurityException | IOException ex) {
+                    Log.e(LOG_TAG, "Cannot open shared preferences using password");
+                    return null;
+                }
+                SupportFactory factory = new SupportFactory(SQLiteDatabase.getBytes(passphrase.toCharArray()));
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .openHelperFactory(factory)
                         .build();
             }
         }
+
         Log.d(LOG_TAG, "Getting the database instance");
         return sInstance;
+
     }
 
     public abstract RPIDao rpiDao();
     public abstract TEKDao tekDao();
+
+    private static String getPassphraseFromSharedPreferences(Context context) throws GeneralSecurityException, IOException {
+        SharedPreferences pref = StorageUtils.getEncryptedSharedPref(
+            context, context.getString(R.string.preference_file_key));
+
+        if (!pref.contains("database-key")) {
+            SharedPreferences.Editor editor = pref.edit();
+            UUID uuid = UUID.randomUUID();
+            editor.putString("database-key", uuid.toString());
+            editor.commit();
+        }
+
+        return pref.getString("database-key", null);
+    }
 }
